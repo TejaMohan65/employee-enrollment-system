c8e58572a4865ea125968379a970234576f17f4f
Teja Mohan <tejavedantham0987@gmail.com>
2026-04-13 11:52:06 +0530

    fixed cors wilcard

--- src/main/java/com/comopany/employee_enrollment/config/SecurityConfig.java
+++ src/main/java/com/comopany/employee_enrollment/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://employee-enrollment-system-e1f9fcn66.vercel.app"));
+        config.setAllowedOriginPatterns(List.of("https://*.vercel.app"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
